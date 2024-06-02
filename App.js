9f051d94061bd5246f35050e5cd87d5d429288ec
Kuwguap <richierodney434@gmail.com>
2024-06-02 21:45:15 +0000

    OTP Authentication
    OTP Authentication application using firebase. Created this for a project I'm currently working on that requires a login or sign up. To eliminate storing passwords, Mobile OTP Authentication is the method i chose to mitigate storing user credentials.

---
+++ App.js
@@ -0,0 +1,33 @@
+import 'react-native-gesture-handler';
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from "./src/Login";
+import Detail from "./src/Detail";
+import Dashboard from './src/Dashboard';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+          <Stack.Screen
+            name="Detail"
+            component={Detail}
+            options={{ headershown: false}}
+          />
+          <Stack.Screen
+            name="Dashboard"
+            component={Dashboard}
+            options={{ headershown: false}}
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
+  ); 
+}
